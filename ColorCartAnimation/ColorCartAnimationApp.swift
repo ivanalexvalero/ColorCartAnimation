2b802b79023c1004eb2fbde6fa40d2af89595f64
ivan <ivanvalero1985@gmail.com>
2022-01-24 16:08:23 -0300

    Initial Commit

---
+++ ColorCartAnimation/ColorCartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCartAnimationApp.swift
+//  ColorCartAnimation
+//
+//  Created by Ivan Valero on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorCartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
